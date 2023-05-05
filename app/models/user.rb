dff12265aac7d31ef3eb3c12623b2f03614191ce
german-zarate <germanzaratevalbuena@gmail.com>
2023-05-06 00:41:58 +0600

    Update schema and user model according to the test cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, confirmation: true
+  validates :name, presence: true
+  validates :avatar, presence: true
+  validates :role, inclusion: { in: %w[user admin] }
 end
