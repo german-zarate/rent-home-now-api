d68560f20788afbbc72cba58bcfd651e1c58d1aa
german-zarate <germanzaratevalbuena@gmail.com>
2023-05-05 11:32:49 -0600

    Validate reservation criterial arguments

---
+++ app/models/reservation_criteria.rb
@@ -0,0 +1,8 @@
+class ReservationCriteria < ApplicationRecord
+
+  validates :time_period, presence: true
+  validates :others_fee, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :rate, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :min_time_period, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :max_guest, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+end
