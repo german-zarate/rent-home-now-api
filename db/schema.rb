6a5fa86db55090b16fd7eb887552f8382d933dee
german-zarate <germanzaratevalbuena@gmail.com>
2023-05-05 22:42:22 +0300

    Add validation for property and address model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,63 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_05_170546) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_05_193345) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "house_number"
+    t.string "street"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.string "zip_code"
+    t.bigint "property_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["property_id"], name: "index_addresses_on_property_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.text "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "source"
+    t.bigint "property_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["property_id"], name: "index_images_on_property_id"
+  end
+
+  create_table "properties", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "no_bedrooms"
+    t.integer "no_baths"
+    t.integer "no_beds"
+    t.float "area"
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_properties_on_category_id"
+    t.index ["user_id"], name: "index_properties_on_user_id"
+  end
+
+  create_table "reservation_criteria", force: :cascade do |t|
+    t.text "time_period"
+    t.float "others_fee"
+    t.integer "min_time_period"
+    t.integer "max_guest"
+    t.float "rate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "property_id", null: false
+    t.index ["property_id"], name: "index_reservation_criteria_on_property_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +83,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_05_170546) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "addresses", "properties"
+  add_foreign_key "images", "properties"
+  add_foreign_key "properties", "categories"
+  add_foreign_key "properties", "users"
+  add_foreign_key "reservation_criteria", "properties"
 end
