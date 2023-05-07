a9dce4f89b2110e7f6e857615ad2906895bbd48b
german-zarate <germanzaratevalbuena@gmail.com>
2023-05-07 21:35:42 +0300

    Add testcase for property model

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Property, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:category) { Category.create(name: "Apartment") }
+  let(:user) { User.create(name: "John Doe", email: "john@example.com", password: "password", role: "user", avatar: "https://randomuser.me/api/portraits/men/1.jpg") }
+
+  before do
+    @property = Property.new(
+      name: "Cozy Apartment",
+      description: "A cozy apartment in the heart of the city",
+      no_bedrooms: 1,
+      no_baths: 1,
+      no_beds: 2,
+      area: 50.0,
+      user: user,
+      category: category
+    )
+  end
+
+   it "is valid with valid attributes" do
+    expect(@property).to be_valid
+  end
+  
+  it "is not valid without a name" do
+    @property.name = nil
+    expect(@property).to_not be_valid
+  end
+  
+  it "is not valid without a description" do
+    @property.description = nil
+    expect(@property).to_not be_valid
+  end
+  
+  it "is not valid without a user" do
+    @property.user = nil
+    expect(@property).to_not be_valid
+  end
+  
+  it "is not valid without a category" do
+    @property.category = nil
+    expect(@property).to_not be_valid
+  end
 end
