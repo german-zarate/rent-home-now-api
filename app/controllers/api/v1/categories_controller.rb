f14b3bde75ab05ff111364eff781a05a486f5689
german-zarate <germanzaratevalbuena@gmail.com>
2023-05-08 06:33:43 -0600

    Add show route for categories response

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::CategoriesController < ApplicationController
     @categories = Category.all
     render json: @categories
   end
+
+  def show
+    @categories = Category.find(params[:id])
+    render json: @categories
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1, defaults: {format: 'json'} do
       resources :properties, only: %i[index show create update destroy]
       resources :reservation_criterias, only: [:index, :show, :create, :update, :destroy]
-      resources :categories, only: [:index]
+      resources :categories, only: [:index, :show]
     end
   end
 
