82f5a2035bf326cd4dd1b6a5ed6156cd8e8718d1
german-zarate <germanzaratevalbuena@gmail.com>
2023-05-09 17:46:36 -0600

    Add category model test

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    @category = Category.create!(name:"Mansion")
+  end
+
+  before { subject.save }
+
+  describe 'Testing Category validations' do
+    it 'Should be invalid without a name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'Should be valid with the correct values' do
+      expect(subject).to be_valid
+    end
+  end
 end
