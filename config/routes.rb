9616d5760490289dbb19d8640690f40336d31ccc
german-zarate <germanzaratevalbuena@gmail.com>
2023-05-09 07:18:19 +0300

    Add addresses controller and its route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
-    namespace :v1, defaults: {format: 'json'} do
-      resources :categories, only: [:index, :show]
+    namespace :v1, defaults: { format: 'json' } do
+      resources :categories, only: %i[index show]
       resources :properties, only: %i[index show create update destroy]
-      resources :reservation_criterias, only: [:index, :show, :create, :update, :destroy]
+      resources :reservation_criterias, only: %i[index show create update destroy]
+      resources :addresses, only: %i[create update destroy]
     end
   end
 
