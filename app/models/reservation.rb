e2045cb3b2c2a9b2ea3413dcd5618b70e851b689
german-zarate <germanzaratevalbuena@gmail.com>
2023-05-15 23:56:05 +0600

    Add set_reservation_price to calculate reservation price and update model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,4 +5,13 @@ class Reservation < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :guests, presence: true
+
+  before_save :set_reservation_price
+
+  private
+
+  def set_reservation_price
+    num_nights = end_date - start_date
+    self.price = (property.reservation_criteria.rate * num_nights) + property.reservation_criteria.others_fee
+  end
 end
