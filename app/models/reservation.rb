09ae049c9cc92f00eac9ee7b25501a85d7184f35
german-zarate <germanzaratevalbuena@gmail.com>
2023-05-15 23:58:42 +0600

    Add method to check if the property is already reserved on the given start and end date

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -9,6 +9,7 @@ class Reservation < ApplicationRecord
   validate :check_guest_capacity
   validate :end_date_after_start_date
   validate :start_date_smaller_than_today
+  validate :property_available_on_reservation_dates
 
   before_save :set_reservation_price
 
@@ -40,5 +41,16 @@ class Reservation < ApplicationRecord
     return unless start_date < Date.today
 
     errors.add(:start_date, 'Start date must be greater than Today')
+  end
+
+  def property_available_on_reservation_dates
+    return unless start_date.present? && end_date.present?
+
+    overlapping_reservations = Reservation.where(property_id:)
+      .where('(start_date <= ? AND end_date >= ?) OR (start_date <= ? AND end_date >= ?)',
+             start_date, start_date, end_date, end_date)
+      .where.not(id:)
+
+    errors.add(:base, 'Property is already reserved for the selected dates') if overlapping_reservations.exists?
   end
 end
