01e2c828cae85abf06828d67d5ab3197df899334
german-zarate <germanzaratevalbuena@gmail.com>
2023-05-15 23:57:03 +0600

    Add check_guest_capacity to check the capacity does not exceeded for reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,6 +6,8 @@ class Reservation < ApplicationRecord
   validates :end_date, presence: true
   validates :guests, presence: true
 
+  validate :check_guest_capacity
+
   before_save :set_reservation_price
 
   private
@@ -13,5 +15,12 @@ class Reservation < ApplicationRecord
   def set_reservation_price
     num_nights = end_date - start_date
     self.price = (property.reservation_criteria.rate * num_nights) + property.reservation_criteria.others_fee
+  end
+
+  def check_guest_capacity
+    property = Property.find_by(id: property_id)
+    return unless property && guests > property.reservation_criteria.max_guest
+
+    errors.add(:max_guest, 'Guests number exceeded the maximum allowed')
   end
 end
