8d0a6def071fa0789502244bab6db856a2eedc7f
german-zarate <germanzaratevalbuena@gmail.com>
2023-05-15 23:57:56 +0600

    Add method for start and end date validation for reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -7,6 +7,8 @@ class Reservation < ApplicationRecord
   validates :guests, presence: true
 
   validate :check_guest_capacity
+  validate :end_date_after_start_date
+  validate :start_date_smaller_than_today
 
   before_save :set_reservation_price
 
@@ -22,5 +24,21 @@ class Reservation < ApplicationRecord
     return unless property && guests > property.reservation_criteria.max_guest
 
     errors.add(:max_guest, 'Guests number exceeded the maximum allowed')
+  end
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    return unless end_date <= start_date
+
+    errors.add(:end_date, 'End date must be after Start date')
+  end
+
+  def start_date_smaller_than_today
+    return if start_date.blank?
+
+    return unless start_date < Date.today
+
+    errors.add(:start_date, 'Start date must be greater than Today')
   end
 end
