f91daf60f4c0c663c4c8f904ffec60a87f1bfcf9
german-zarate <germanzaratevalbuena@gmail.com>
2023-05-18 00:20:08 +0300

    add swagger for an address

--- spec/requests/api/v1/addresses_spec.rb
+++ spec/requests/api/v1/addresses_spec.rb
@@ -1,10 +1,13 @@
 require 'swagger_helper'
 
-RSpec.describe 'api/v1/users', type: :request do
-  path '/api/v1/users' do
-    get('list users') do
-      tags 'Users'
+RSpec.describe 'api/v1/addresses', type: :request do
+
+  path '/api/v1/addresses' do
+
+    post('create address') do
+      tags 'Addresses'
       response(200, 'successful') do
+
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -17,12 +20,12 @@ RSpec.describe 'api/v1/users', type: :request do
     end
   end
 
-  path '/api/v1/users/{id}' do
+  path '/api/v1/addresses/{id}' do
     # You'll want to customize the parameter types...
     parameter name: 'id', in: :path, type: :string, description: 'id'
 
-    get('show user') do
-      tags 'Users'
+    patch('update address') do
+      tags 'Addresses'
       response(200, 'successful') do
         let(:id) { '123' }
 
@@ -37,8 +40,8 @@ RSpec.describe 'api/v1/users', type: :request do
       end
     end
 
-    patch('update user') do
-      tags 'Users'
+    put('update address') do
+      tags 'Addresses'
       response(200, 'successful') do
         let(:id) { '123' }
 
@@ -53,43 +56,11 @@ RSpec.describe 'api/v1/users', type: :request do
       end
     end
 
-    put('update user') do
-      tags 'Users'
+    delete('delete address') do
+      tags 'Addresses'
       response(200, 'successful') do
         let(:id) { '123' }
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-
-    delete('delete user') do
-      tags 'Users'
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-  end
-
-  path '/api/v1/auth/sign_up' do
-    post('create user') do
-      tags 'Users'
-      response(200, 'successful') do
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
